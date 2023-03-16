38c3b7b5863e9ccfde8e59fdd4f48d990495582a
zverosoff <raven-udomlya@mail.ru>
2023-03-16 19:52:34 +0300

    Configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import mkcert from 'vite-plugin-mkcert';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    https: 'local.dev',
+    https: true,
     port: '8888',
+    host: 'local.dev',
   },
   plugins: [mkcert()],
 });

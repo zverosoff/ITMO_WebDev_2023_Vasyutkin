0b662c886cce30590c548e818a5090700fb851e1
zverosoff <raven-udomlya@mail.ru>
2023-04-08 19:20:10 +0300

    JS_class add button to task

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import 'uno.css';
+import presetIcons from '@unocss/preset-icons';
 import '@unocss/reset/tailwind.css';
+
+const domBtnCreateTask = document.getElementById('btnCreateTask');
+const domPopupCreateTask = document.getElementById('popupCreateTask');
+
+domBtnCreateTask.onclick = (e) => {
+  console.log(e);
+  domPopupCreateTask.classList.remove('hidden');
+
+  const domClouseCreateTaskPopup = document.getElementById('btnClouseCreateTaskPopup');
+  domClouseCreateTaskPopup.onclick = () => {
+    domPopupCreateTask.classList.add('hidden');
+    domClouseCreateTaskPopup.onclick = null;
+  };
+};

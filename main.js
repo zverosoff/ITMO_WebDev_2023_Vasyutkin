6a8694b13f40f4f8032ce83067387df86980c692
zverosoff <raven-udomlya@mail.ru>
2023-04-13 14:39:20 +0300

    Complite Lesson-16

--- main.js
+++ main.js
@@ -1,94 +1,33 @@
-import 'uno.css';
-import '@unocss/reset/tailwind.css';
-import Dom from './src/constants/DOM';
-import { randomString } from './src/utils/srtingUtils';
+import { Planet, Position } from './src/solar-system';
 
-class TaskVO {
-  constructor(title, date, tag) {
-    this.title = title;
-    this.date = date;
-    this.tag = tag;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const centerPosition = new Position(canvas.width / 2, canvas.height / 2);
+const sun = new Planet(centerPosition, 1.1, 10, 'red', 50);
+const earth = new Planet(sun.position, 2.2, 10, 'blue', 100);
+
+const planets = [
+  sun,
+  earth,
+  new Planet(centerPosition, 0.3, 20, 'green', 120),
+  new Planet(centerPosition, 0.4, 50, 'gold', 170),
+];
+
+let planet;
+
+const render = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const planetIndex in planets) {
+    planet = planets[planetIndex];
+    planet.move();
+    planet.render(ctx);
   }
-}
-
-let inpTitle = document.getElementById('inpTitle');
-let inpDate = document.getElementById('inpDate');
-let countries = document.getElementById('countries');
-
-const task = new TaskVO('Read', Date.now());
-
-const getDOM = (id) => document.getElementById(id);
-const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
-
-const domTask = getDOM(Dom.Template.TASK);
-
-const tasks = [];
-
-const selectTagsArrays = ['Web', 'Update', 'Desing', 'Content'];
-const selectList = getDOM('tags');
-
-function tagsList() {
-  let tag = '';
-  selectTagsArrays.forEach((item) => {
-    tag += `
-    <option value="${item}">${item}</option>
-    `;
-    selectList.innerHTML = tag;
-  });
-}
-
-getDOM(Dom.Button.CREATE_TASK).onclick = () => {
-  console.log('> domPopupCreateTask.classList');
-
-  const domPopupCreateTask = getDOM(Dom.Popup.CREATE_TASK);
-  const domBtnClose = QUERY(domPopupCreateTask, Dom.Button.CLOSE_POPUP_CREATE_TASK);
-  const domBtnConfirm = QUERY(domPopupCreateTask, Dom.Button.POPUP_CREATE_TASK_CONFIRM);
-  domPopupCreateTask.classList.remove('hidden');
-
-  const onClousePopup = () => {
-    domPopupCreateTask.classList.add('hidden');
-    domBtnClose.onclick = null;
-    domBtnConfirm.onclick = null;
-  };
-
-  domBtnClose.onclick = onClousePopup;
-
-  let acceptData = () => {
-    tasks.push({
-      text: inpTitle.value,
-      date: inpDate.value,
-      description: countries.value,
-    });
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    console.log(taskVO);
-  };
-
-  let resetForm = () => {
-    inpTitle.value = '';
-    inpDate.value = '';
-    tags.value = '';
-  };
-
-  domBtnConfirm.onclick = () => {
-    console.log('conmirm');
-    const taskVO = new TaskVO(inpTitle.value, inpDate.value, tags.value);
-    const taskView = domTask.cloneNode(true);
-
-    QUERY(taskView, Dom.Template.Task.TITLE).innerText = taskVO.title;
-    QUERY(taskView, Dom.Template.Task.DATE).innerText = taskVO.date;
-    QUERY(taskView, Dom.Template.Task.COUTRIES).innerText = taskVO.tag;
-
-    domTask.parentNode.prepend(taskView);
-    tasks.push(taskVO);
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    console.log('confirm', taskVO);
-
-    onClousePopup();
-    resetForm();
-  };
-  tagsList();
+  window.requestAnimationFrame(render);
 };
+
+window.requestAnimationFrame(render);
